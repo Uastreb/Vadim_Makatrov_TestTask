996a09740807d49b71bb9051c108dde0ad7d0040
Vadim_Proger_3000 <vadim.makatrov@mail.ru>
2019-05-21 21:55:07 +0300

    ф

---
+++ Vadim_Makatrov_TestTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vadim_Makatrov_TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataOperations dataOperations = new DataOperations();
+            dataOperations.Menu();
+
+            Console.ReadKey();
+        }
+    }
+}
