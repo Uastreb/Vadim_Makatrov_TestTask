3f4ce6f0460c5241183ed68a15ac2bca89a9b1b6
Vadim_Proger_3000 <vadim.makatrov@mail.ru>
2019-05-21 21:49:35 +0300

    create_rep

--- Vadim_Makatrov_TestTask/Program.cs
+++ Vadim_Makatrov_TestTask/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,10 @@ namespace Vadim_Makatrov_TestTask
     {
         static void Main(string[] args)
         {
+            DataOperations dataOperations = new DataOperations();
+            dataOperations.Menu();
+
+            Console.ReadKey();
         }
     }
 }
