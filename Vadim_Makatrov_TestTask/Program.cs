4c385de0cd806c9b3bec8a0abdf4ada074aa9bc3
Vadim_Proger_3000 <vadim.makatrov@mail.ru>
2019-05-21 13:56:35 +0300

    Добавьте файлы проекта.

---
+++ Vadim_Makatrov_TestTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vadim_Makatrov_TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
